4f590a9bf0435c5fb0edebef072c581642dfb198
Paul Downey <psd@whatfettle.com>
2019-12-04 07:05:42 +0000

    Scrape data.gov.uk for brownfield site pages
    The idea is to automatically find mentions of URLs listed on
    https://data.gov.uk (a small subset of those found by hand).
    
    Need to scrape the site becacuse the data dumps no longer exist
    and paging the search API is broken.

---
+++ bin/data-gov-uk.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import os
+from pyquery import PyQuery
+from urllib.parse import urljoin
+import requests
+
+def save(path, data):
+    print(path)
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    with open(path, "wb") as f:
+        f.write(data)
+
+url = 'https://data.gov.uk/search?q=brownfield'
+
+while url:
+    print("#", url)
+    d = PyQuery(url=url)
+
+    for link in d.items('.dgu-results__result a'):
+        href = link.attr['href']
+        key = href.split("/")[2]
+        response = requests.get("https://ckan.publishing.service.gov.uk/api/3/action/package_show?id=%s"%(key))
+        save("content/%s"%(key), response.content)
+        print(key)
+
+    next_url=d("a[rel='next']").attr['href']
+    url=urljoin(url, next_url) if next_url else None
