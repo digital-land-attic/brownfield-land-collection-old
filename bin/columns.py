fb1226550c481f08de2d932f6c52a327d8b84037
Paul Downey <psd@whatfettle.com>
2020-01-21 23:52:21 +0000

    Count comes first

--- bin/columns.py
+++ bin/columns.py
@@ -11,7 +11,7 @@ for path in glob.glob("var/normalised/*.csv"):
     for field in reader.fieldnames:
         column[field] = column.get(field, 0) + 1
 
-    writer = csv.DictWriter(open(sys.argv[1], "w", newline=""), fieldnames=["name", "count"])
+    writer = csv.DictWriter(open(sys.argv[1], "w", newline=""), fieldnames=["count", "name"])
     writer.writeheader()
     for name in sorted(column):
         writer.writerow({ "name": name, "count": column[name] })
