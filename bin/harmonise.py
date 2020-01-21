a25a20f911ddff3b06888adb8cb4fab03e26d0de
Paul Downey <psd@whatfettle.com>
2020-01-21 22:51:27 +0000

    Harmonise data formats, use schema

--- bin/harmonise.py
+++ bin/harmonise.py
@@ -9,40 +9,93 @@
 import sys
 import re
 import csv
+import json
+from datetime import datetime
 
 
+schema = json.load(open("schema/brownfield-land.json"))
+fieldnames = [field["name"] for field in schema["fields"]]
 
-# TBD: load full schema.json
-schema = {
-    "fields": [
-        {
-            "name": "OrganisationURI",
-            "title": "Organisation URI",
-            "typos": ["OrganistionURI", "LPA URL", "OPEN DATA WEBSITE", "Organisati"],
-        },
-        {
-            "name": "SiteReference",
-            "title": "Site reference",
-            "typos": ["LA SITE REF", "SiteRefere"],
-        },
-        {"name": "GeoX",},
-        {"name": "GeoY",},
-        {"name": "Hectares", "typos": ["AREA (HA)"]},
-        {"name": "FirstAddedDate", "typos": ["FirstAdded"]},
-        {"name": "LastUpdatedDate", "typos": ["LastUpdate"]},
-        {"name": "EndDate"},
-    ]
+# hard-coded for now
+fieldnames = [
+    "OrganisationURI",
+    "SiteReference",
+    "GeoX",
+    "GeoY",
+    "Hectares",
+    "FirstAddedDate",
+    "LastUpdatedDate",
+    "EndDate",
+]
+
+fields = {
+    field["name"]: field for field in schema["fields"] if field["name"] in fieldnames
 }
+
 
 pattern = re.compile(r"[^a-z0-9]")
 
-def normalise(field, value):
+
+def normalise_date(value):
+    value = value.strip(' ",')
+
+    # all of these patterns have been used!
+    if value in ["", "N/A", "#N/A", "???"]:
+        return ""
+
+    for pattern in [
+        "%Y-%m-%d",
+        "%Y%m%d",
+        "%Y-%m-%dT%H:%M:%S.000Z",
+        "%Y-%m-%dT%H:%M:%SZ",
+        "%Y-%m-%dT%H:%M:%S",
+        "%Y-%m-%d %H:%M:%S",
+        "%d/%m/%Y %H:%M:%S",
+        "%d/%m/%Y %H:%M",
+        "%Y",
+        "%Y.0",
+        "%Y/%m/%d",
+        "%Y-%d-%m",
+        "%Y.%m.%d",
+        "%d-%m-%Y",
+        "%d-%b-%Y",
+        "%d-%m-%y",
+        "%d-%m-%Y",
+        "%d.%m.%y",
+        "%d/%m/%Y",
+        "%d/%m/%y",
+        "%m/%d/%Y",
+        "%d-%b-%y",
+        "%d %B %Y",
+        "%Y %m %d",
+        "%b %d, %Y",
+        "%b %d, %y",
+        "%b-%y",
+    ]:
+        try:
+            date = datetime.strptime(value, pattern)
+            return date.strftime("%Y-%m-%d")
+        except ValueError:
+            pass
+
+    return ""
+
+
+def normalise_uri(value):
+    # some URIs have line-breaks and spaces
+    return "".join(value.split())
+
+
+def normalise(fieldname, value):
     value = value or ""
-    # TBD: deduce type/format from schema
-    if field == "OrganisationURI":
-        value = "".join(value.split())
-    if field.endswith("Date"):
-        value = value.strip('"')
+    field = fields[fieldname]
+
+    if field.get("format", "") == "uri":
+        return normalise_uri(value)
+
+    if field.get("type", "") == "date":
+        return normalise_date(value)
+
     return value
 
 
@@ -51,16 +104,16 @@ def name(name):
 
 
 if __name__ == "__main__":
-    fieldnames = [field["name"] for field in schema["fields"]]
 
     # index of typos
     typos = {}
-    for field in schema["fields"]:
-        typos[name(field["name"])] = field["name"]
+    for fieldname in fieldnames:
+        field = fields[fieldname]
+        typos[name(fieldname)] = fieldname
         if "title" in field:
-            typos[name(field["title"])] = field["name"]
+            typos[name(field["title"])] = fieldname
         for typo in field.get("typos", []):
-            typos[name(typo)] = field["name"]
+            typos[name(typo)] = fieldname
 
     reader = csv.DictReader(open(sys.argv[1], newline=""))
 
