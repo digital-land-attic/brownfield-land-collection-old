6befed1de37cc4f071e0db2f5676ca6eaa9be912
Paul Downey <psd@whatfettle.com>
2020-02-08 10:06:24 +0000

    Count oncemake

--- bin/columns.py
+++ bin/columns.py
@@ -8,12 +8,13 @@ column = {}
 
 for path in glob.glob("var/normalised/*.csv"):
     reader = csv.DictReader(open(path, newline=""))
-    for field in reader.fieldnames:
-        column[field] = column.get(field, 0) + 1
+    if reader.fieldnames:
+        for field in reader.fieldnames:
+            column[field] = column.get(field, 0) + 1
 
-    writer = csv.DictWriter(
-        open(sys.argv[1], "w", newline=""), fieldnames=["column", "count"]
-    )
-    writer.writeheader()
-    for name in sorted(column):
-        writer.writerow({"column": name, "count": column[name]})
+writer = csv.DictWriter(
+    open(sys.argv[1], "w", newline=""), fieldnames=["column", "count"]
+)
+writer.writeheader()
+for name in sorted(column):
+    writer.writerow({"column": name, "count": column[name]})
