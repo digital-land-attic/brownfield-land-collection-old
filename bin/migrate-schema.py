9d3f81eada02e23a089734e0d36ab28c0eb79594
Jon Bramley <jon.bramley@sainsburys.co.uk>
2020-06-02 16:48:02 +0100

    Extract enums to enum.csv

--- bin/migrate-schema.py
+++ bin/migrate-schema.py
@@ -9,21 +9,27 @@ schema = json.load(open("schema/brownfield-land.json"))
 
 
 datatypes = set()
+enums = []
 fields = []
 
 from pprint import pprint
 
 for field in schema["fields"]:
-    datatypes.add(field.get("type", None))
+    datatype = field.get("format", None) or field.get("type", "string")
+    datatypes.add(datatype)
     fields.append(
         {
-            "name": field["name"],
-            "datatype": field.get("type", None),
+            "field": field["name"],
+            "name": field["title"],
+            "datatype": datatype,
             "description": field["description"],
             "parent-field": "",
             "replacement-field": "",
         }
     )
+
+    if "constraints" in field and "enum" in field["constraints"]:
+        enums.append((field["name"], field["constraints"]["enum"]))
 
 with open("schema/schema.csv", "w") as schema_file:
     schema_csv = csv.DictWriter(schema_file, fieldnames=["name", "field"])
@@ -43,7 +49,7 @@ with open("schema/schema-field.csv", "w") as schema_field_file:
             {
                 "schema": "brownfield-land",
                 "field": field["name"],
-                "cardinality": "",
+                "cardinality": "1",
                 "description": field["description"],
             }
         )
@@ -55,6 +61,7 @@ with open("schema/field.csv", "w") as field_file:
     field_csv = csv.DictWriter(
         field_file,
         fieldnames=[
+            "field",
             "name",
             "datatype",
             "description",
@@ -77,3 +84,13 @@ with open("schema/datatype.csv", "w") as datatype_file:
         datatype_csv.writerow({"name": datatype})
         datatype_count += 1
 print(f"{datatype_count} datatypes written to schema/datatype.csv")
+
+enum_count = 0
+with open("schema/enum.csv", "w") as enum_file:
+    enum_csv = csv.DictWriter(enum_file, fieldnames=["field", "value"])
+    enum_csv.writeheader()
+    for field, values in enums:
+        for value in values:
+            enum_csv.writerow({"field": field, "value": value})
+            enum_count += 1
+print(f"{enum_count} enums written to schema/enum.csv")
